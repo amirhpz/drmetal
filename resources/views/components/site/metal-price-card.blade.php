13662fe0120440412fac9fa4851a815ee862eb54
amir <amirhpz2000@gmail.com>
2026-07-04 13:56:14 +0330

    v0.52

--- resources/views/components/site/metal-price-card.blade.php
+++ resources/views/components/site/metal-price-card.blade.php
@@ -3,7 +3,6 @@
 <article @class(['price-card', 'is-up' => $price->isUp(), 'is-down' => $price->isDown(), 'is-stale' => $price->is_stale])>
     <div class="price-card-head">
         <div>
-            <span class="symbol">{{ $price->symbol }}</span>
             <h3>{{ $price->name }}</h3>
         </div>
         <span class="price-status">{{ $price->is_stale ? 'آخرین داده' : 'به‌روز' }}</span>
